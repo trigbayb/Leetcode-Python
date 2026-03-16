19fe962f3671388f7a0919368f24a657d96ee210
Аnastasia Baybikova <52564341+trigbayb@users.noreply.github.com>
2026-03-16 21:16:02 +0300

    Time: 43 ms (10.98%), Space: 19.7 MB (40.33%) - LeetHub

---
+++ 0748-shortest-completing-word/0748-shortest-completing-word.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        count=10001
+        for i in words:
+            flag=True
+            # print(i)
+            k=i
+            for j in licensePlate:
+                print(i, j, j.isnumeric() or j==" ", k)
+                if j.lower() in k:
+                    k=k.replace(j.lower(), "", 1)
+                    print(k)
+                elif j.isnumeric() or j==" ":
+                    continue
+                else:
+                    flag=False
+                    break
+            print(flag, i,k)
+            if flag==True and len(i)<count:
+                count=len(i)
+                output=i
+        return output
+            
